e3b4568743833f285541e89605afa4419945db96
Miki <jaworski.miki98@gmail.com>
2021-05-14 13:20:06 +0200

    Llista de canvis
    - A la pantalla principal si la política no esta acceptada obre la pantalla per acceptar-la.
    - Creada la pantalla de política.

--- RestaSearch/app/src/main/java/com/cursfundacionesplai/restasearch/MainActivity.java
+++ RestaSearch/app/src/main/java/com/cursfundacionesplai/restasearch/MainActivity.java
@@ -2,13 +2,26 @@ package com.cursfundacionesplai.restasearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        boolean acceptedPolicy = prefs.getBoolean("key_shared_prefs_policy", false);
+
+        if (!acceptedPolicy) {
+            startActivity(new Intent(this, PoliticaActivity.class));
+        }
     }
 }
---
+++ RestaSearch/app/src/main/java/com/cursfundacionesplai/restasearch/PoliticaActivity.java
@@ -0,0 +1,44 @@
+package com.cursfundacionesplai.restasearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class PoliticaActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+
+    CheckBox checkPolicy;
+    Button accept;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_politica);
+
+        prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        editor = prefs.edit();
+
+        checkPolicy = findViewById(R.id.box_policy);
+        accept = findViewById(R.id.btn_accept);
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isChecked = checkPolicy.isChecked();
+                if (isChecked) {
+                    editor.putBoolean("key_shared_prefs_policy", true);
+                    editor.apply();
+
+                    finish();
+                }
+            }
+        });
+    }
+}
